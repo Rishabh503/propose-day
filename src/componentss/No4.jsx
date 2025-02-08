02b2e83e1e7e7181fcee0c0f6c0f6df8458fe6f5
Rishabh503 <rishabhtripathi2022@gmail.com>
2025-02-08 20:52:46 +0530

    fixing bugs

--- src/componentss/No4.jsx
+++ src/componentss/No4.jsx
@@ -8,7 +8,8 @@ const No4 = () => {
         <div className='min-h-screen w-full flex flex-col  items-center justify-center bg-red-50'>
             <div className='p-10 bg-yellow-50 w-auto h-auto'>
                 <div>
-                <div  data-postid="9582531637857092465" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/robert-cat-draw-gif-9582531637857092465">Robert Cat GIF</a>from <a href="https://tenor.com/search/robert-gifs">Robert GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+                <div class="tenor-gif-embed" 
+                 data-postid="9582531637857092465" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/robert-cat-draw-gif-9582531637857092465">Robert Cat GIF</a>from <a href="https://tenor.com/search/robert-gifs">Robert GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
                     <h1>
                         WILL  YOU BE MY VALENTINE 🌹
                     </h1>
